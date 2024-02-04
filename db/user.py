b68badb153d924be084f5d37ff7c32efdf80a152
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2024-02-04 18:02:16 +0300

    Added user.py

--- db/user.py
+++ db/user.py
@@ -1,3 +1,13 @@
 from base import BaseModel
+from sqlalchemy import Column, Integer, VARCHAR, DATE
+
 
 class User(BaseModel):
+    __tablename__ = 'users'
+    user_id = Column(Integer, unique=True, nullable=False)
+    user_name = Column(VARCHAR(32), unique=False, nullable=True)
+    reg_date = Column()
+    upd_date = Column()
+
+    def __str__(self) -> str:
+        return f'User: {self.user_id}'
