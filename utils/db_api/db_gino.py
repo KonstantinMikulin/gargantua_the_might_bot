ea5e9db757bc828fc89be4e87ed6fe68431724b5
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2024-02-07 14:43:58 +0300

    Added db_gino.py

---
+++ utils/db_api/db_gino.py
@@ -0,0 +1,22 @@
+import sqlalchemy as sa
+
+from gino import Gino
+from typing import List
+from sqlalchemy import Column, BigInteger, String
+
+db = Gino()
+
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    def __str__(self):
+        model = self.__class__.__name__
+        table: sa.Table = sa.inspect(self.__class__)
+        primary_key_columns: List[sa.Column] = table.primary_key.columns
+        values = {
+            column.name: getattr(self, self._column_name_map[column.name])
+            for column in primary_key_columns
+        }
+        values_str = " ".join(f"{name}={value!r}" for name, value in values.items())
+        return f"<{model} {values_str}>"
